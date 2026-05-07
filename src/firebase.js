9edef87152ee54d2b29cf5e346e333b065bc512f
eternmecontacto <eternmecontacto@gmail.com>
2026-05-07 12:31:17 +0200

    fix: move firebase keys to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,12 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDmrzU3THFulnUxdmzR56dKnqKBY0pIBIo",
-  authDomain: "melina-crm.firebaseapp.com",
-  projectId: "melina-crm",
-  storageBucket: "melina-crm.firebasestorage.app",
-  messagingSenderId: "730484335539",
-  appId: "1:730484335539:web:33ed818827082d6501a1c6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
-
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
